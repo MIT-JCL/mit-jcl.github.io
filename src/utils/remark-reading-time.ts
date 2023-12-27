6f70f980c8f461b2211029f6c8037b39cf15988b
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2023-12-27 02:03:48 -0600

    add types

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -1,10 +1,12 @@
 import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
+import type { RemarkPlugin } from "@astrojs/markdown-remark";
 
-export function remarkReadingTime() {
+export const remarkReadingTime: RemarkPlugin = () => {
 	return function (tree, { data }) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
+		// @ts-expect-error: astro types are annoying me but it works i promise </3
 		data.astro.frontmatter.minutesRead = readingTime.text;
 	};
-}
+};
