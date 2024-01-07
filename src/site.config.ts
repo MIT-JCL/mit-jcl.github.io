057dc90d2978c18f27e2c2eecc852d2446f320dc
qperian <57880014+qperian@users.noreply.github.com>
2024-01-07 01:08:13 -0500

    Changed "posts" to "statements" and updated formatting for News features (added NewsClip.astro)

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,46 +1,46 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "MIT J4C",
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "MIT Jews for Ceasefire",
-	// Meta property used as the default description meta property
-	description: "A website for MIT Jews for Ceasefire",
-	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-US",
-	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_US",
-	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
-	date: {
-		locale: "en-US",
-		options: {
-			day: "numeric",
-			month: "short",
-			year: "numeric",
-		},
-	},
-	// webmentions: {
-	// 	  link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
-	// },
+  // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+  author: "MIT J4C",
+  // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+  title: "MIT Jews for Ceasefire",
+  // Meta property used as the default description meta property
+  description: "A website for MIT Jews for Ceasefire",
+  // HTML lang property, found in src/layouts/Base.astro L:18
+  lang: "en-US",
+  // Meta property, found in src/components/BaseHead.astro L:42
+  ogLocale: "en_US",
+  // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+  date: {
+    locale: "en-US",
+    options: {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    },
+  },
+  // webmentions: {
+  // 	  link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+  // },
 };
 
 // Used to generate links in both the Header & Footer.
 export const menuLinks: Array<{ title: string; path: string }> = [
-	{
-		title: "Home",
-		path: "/",
-	},
-	{
-		title: "About",
-		path: "/about/",
-	},
-	{
-		title: "Media",
-		path: "/media/",
-	},
-	{
-		title: "Blog",
-		path: "/posts/",
-	},
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about/",
+  },
+  {
+    title: "Media",
+    path: "/media/",
+  },
+  {
+    title: "Statements",
+    path: "/posts/",
+  },
 ];
