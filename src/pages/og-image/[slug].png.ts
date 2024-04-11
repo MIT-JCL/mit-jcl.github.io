7180d772692c04609d947c8bea8c2a8e76ae02a5
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-04-11 16:59:51 -0400

    remove cactus

--- src/pages/og-image/[slug].png.ts
+++ src/pages/og-image/[slug].png.ts
@@ -37,7 +37,7 @@ const markup = (title: string, pubDate: string) =>
 		</div>
 		<div tw="flex items-center justify-between w-full p-10 border-t border-[#2bbc89] text-xl">
 			<div tw="flex items-center">
-				<svg height="60" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 272 480">
+				<!-- <svg height="60" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 272 480">
 					<path
 						d="M181.334 93.333v-40L226.667 80v40l-45.333-26.667ZM136.001 53.333 90.667 26.667v426.666L136.001 480V53.333Z"
 						fill="#B04304"
@@ -51,7 +51,7 @@ const markup = (title: string, pubDate: string) =>
 						fill="#53C68C"
 					></path>
 					<path d="M45.334 240 0 213.334v120L45.334 360V240Z" fill="#B04304"></path>
-				</svg>
+				</svg> -->
 				<p tw="ml-3 font-semibold">${siteConfig.title}</p>
 			</div>
 			<p>by ${siteConfig.author}</p>
