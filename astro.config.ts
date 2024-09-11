791b1749209ef967fb2ea0d54ae972ae0690944f
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-09-11 11:37:44 -0400

    Change url

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://mit-j4c.github.io/",
+	site: "https://mit-jcl.github.io/",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		rehypePlugins: [
